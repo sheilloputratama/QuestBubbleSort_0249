d07e941590979f739acd81b4cca6b7439f2dacbd
sheilloputratama <sheilloggendut@gmail.com>
2025-03-25 13:35:23 +0700

    meambah validasi jumlah element array

--- pertemuan 4 online/contoh_insertionsort.cpp
+++ pertemuan 4 online/contoh_insertionsort.cpp
@@ -7,3 +7,17 @@ int n; // membuat variable inputan n
 void input()
 { // procedure input
 while (true)
+{
+    cout << "masukkan jumlah ddata pada array : "; // membuat inputan jumlah element array
+    cin >> n; // memanggil variable inputan n
+    
+    if (n <= 20)
+    {// membuat kondisi n tidak lebih dari 20
+        break;
+    }
+    else
+    {
+        cout << "\nArray yang anda masukkan maksimal 20 element.\n"; //menampilkan pesan jikan data lebih dari 20
+    }
+    }
+    
